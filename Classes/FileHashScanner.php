d157326045e9722ea37332718672578ef727d488
Jan Matthiesen <jmatthiesen@N68S3.(none)>
2013-04-09 15:53:16 +0200

    Extend scanner class with filetypes (strict | exclude)

--- Classes/FileHashScanner.php
+++ Classes/FileHashScanner.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Class: FileHashScanner
+ * Version: 1.0.0
+ */
+
 class FileHashScanner
 {
 
@@ -7,6 +12,11 @@ class FileHashScanner
     private $_changedFiles = array();
     private $_deletedFiles = array();
     private $_excludeFiles = array();
+    private $_filetypes = array(
+        'htm', 'html', 'phtml', 'xml', 'js', 'swf', 'php', 'php3', 'php4', 'php5'
+    );
+    private $_regex = '';
+    private $_negateRegex = false;
 
     private $_logfile = '';
     private $_procCounter = 0;
@@ -15,25 +25,34 @@ class FileHashScanner
         $this->_logfile = strftime('%Y-%m-%d_%H%M%S') . '.log';
     }
 
-    private function initFilesCache(array $exclude = array()) {
+    private function init(array $exclude = array(), array $types = array(), $negate = false) {
         $this->_newFiles = array();
         $this->_changedFiles = array();
         $this->_deletedFiles = array();
         $this->_excludeFiles = $exclude;
+        if (0 < count($types)) {
+            $this->_filetypes = $types;
+        }
+        $this->_filetypes = array_map('preg_quote', $this->_filetypes);
+        $this->_regex = '/^(' . implode('|', $this->_filetypes) . ')$/';
+        $this->_negateRegex = (bool) $negate;
+        unset($negate);
         return $this;
     }
 
-    public function doScan($originDirectory, array $exclude = array(), $cacheDirectory = './cache', $logsDirectory = './logs') {
+    public function doScan($originDirectory, array $exclude = array(), array $types = array(), $negate = false, $cacheDirectory = './cache', $logsDirectory = './logs') {
         ++ $this->_procCounter;
         $originDirectory = rtrim($originDirectory, '/') . '/';
         $cacheDirectory = rtrim($cacheDirectory, '/') . '/';
         $logsDirectory = rtrim($logsDirectory, '/') . '/';
-        foreach ($exclude as &$entry) {
+        foreach ($exclude as $key => &$entry) {
             if ('/' !== $entry{0}) {
-                $entry = realpath($originDirectory . $entry);
+                if (false === $entry = realpath($originDirectory . $entry)) {
+                    unset($exclude[$key]);
+                }
             }
         }
-        return $this->initFilesCache($exclude)
+        return $this->init($exclude, $types, $negate)
             ->scanNewAndChangedFiles($originDirectory, $cacheDirectory)
             ->scanDeletedFiles($originDirectory, $cacheDirectory)
             ->generateStatistics($originDirectory, $logsDirectory);
@@ -42,7 +61,7 @@ class FileHashScanner
     private function scanNewAndChangedFiles($originDirectory, $cacheDirectory) {
         $initCacheDirectory = $cacheDirectory . $originDirectory;
         if (! is_dir($initCacheDirectory)) {
-            mkdir($initCacheDirectory, 0755, TRUE);
+            mkdir($initCacheDirectory, 0755, true);
         }
         unset($initCacheDirectory);
 
@@ -54,29 +73,31 @@ class FileHashScanner
                     $cacheEntry = $cacheDirectory . $pathToEntry;
                     if (is_dir($pathToEntry)) {
                         if (! is_dir($cacheEntry)) {
-                            mkdir($cacheEntry, 0755, TRUE);
+                            mkdir($cacheEntry, 0755, true);
                             $this->_newFiles[] = $pathToEntry;
                         }
                         $this->scanNewAndChangedFiles($pathToEntry, $cacheDirectory);
                     }
                     else {
-                        $cacheEntry .= '.md5';
-                        $newMd5Hash = md5_file($pathToEntry);
-                        $updateMd5HashFile = FALSE;
-                        if (file_exists($cacheEntry)) {
-                            $oldMd5Hash = trim(file_get_contents($cacheEntry));
-                            if ($updateMd5HashFile = ($newMd5Hash !== $oldMd5Hash)) {
-                                $this->_changedFiles[] = $pathToEntry;
+                        if ($this->validFiletype($pathToEntry)) {
+                            $cacheEntry .= '.md5';
+                            $newMd5Hash = md5_file($pathToEntry);
+                            $updateMd5HashFile = false;
+                            if (file_exists($cacheEntry)) {
+                                $oldMd5Hash = trim(file_get_contents($cacheEntry));
+                                if ($updateMd5HashFile = ($newMd5Hash !== $oldMd5Hash)) {
+                                    $this->_changedFiles[] = $pathToEntry;
+                                }
                             }
+                            else {
+                                $this->_newFiles[] = $pathToEntry;
+                                $updateMd5HashFile = true;
+                            }
+                            if ($updateMd5HashFile) {
+                                file_put_contents($cacheEntry, $newMd5Hash);
+                            }
+                            $this->_scannedFiles[] = $pathToEntry;
                         }
-                        else {
-                            $this->_newFiles[] = $pathToEntry;
-                            $updateMd5HashFile = TRUE;
-                        }
-                        if ($updateMd5HashFile) {
-                            file_put_contents($cacheEntry, $newMd5Hash);
-                        }
-                        $this->_scannedFiles[] = $pathToEntry;
                     }
                 }
             }
@@ -116,7 +137,7 @@ class FileHashScanner
 
         if (0 < count($this->_deletedFiles)) {
             foreach ($this->_deletedFiles as $cacheEntry => $originEntry) {
-                switch (TRUE) {
+                switch (true) {
                     case is_file($cacheEntry):
                         unlink($cacheEntry);
                         break;
@@ -135,13 +156,25 @@ class FileHashScanner
         foreach ($this->_excludeFiles as $exclude) {
             $length = strlen($exclude);
             if ($length <= strlen($entry) && substr($entry, 0, $length) === $exclude) {
-                return TRUE;
+                return true;
             }
         }
-        return FALSE;
+        return false;
     }
 
-    private function rmdir($directory, $recursive = TRUE) {
+    private function validFiletype($entry) {
+        if (is_file($entry)) {
+            $ext = strtolower(pathinfo($entry,  PATHINFO_EXTENSION));
+            if (1 < strlen($ext)) {
+                $status = preg_match($this->_regex, $ext);
+                if ($this->_negateRegex) { $status = ! $status; }
+                return $status;
+            }
+        }
+        return false;
+    }
+
+    private function rmdir($directory, $recursive = true) {
         $CurrentWorkingDirectory = dir($directory);
 
         while ($entry = $CurrentWorkingDirectory->read()) {
@@ -161,11 +194,29 @@ class FileHashScanner
         unset($CurrentWorkingDirectory);
     }
 
+    private function resolveType($entry, $wrap = '|') {
+        $attr = false;
+        switch (true) {
+            case is_dir($entry):
+                $attr = 'D';
+                break;
+
+            case is_link($entry):
+                $attr = 'L';
+                break;
+
+            case is_file($entry):
+                $attr = 'F';
+                break;
+        }
+        return str_replace('|', $attr, $wrap);
+    }
+
     private function generateStatistics($originDirectory, $logsDirectory) {
         // $initLogsDirectory = $logsDirectory . $originDirectory;
         $initLogsDirectory = $logsDirectory;
         if (! is_dir($initLogsDirectory)) {
-            mkdir($initLogsDirectory, 0755, TRUE);
+            mkdir($initLogsDirectory, 0755, true);
         }
         $logfile = $this->_logfile;
         $pathToLogfile = rtrim($initLogsDirectory, '/') . '/' . $logfile;
@@ -176,26 +227,26 @@ class FileHashScanner
         }
         file_put_contents($pathToLogfile, 'Scanning directory "' . $originDirectory . '"' . chr(10), FILE_APPEND);
 
-        file_put_contents($pathToLogfile, chr(10) . 'New files (' . count($this->_newFiles) . '):' . chr(10), FILE_APPEND);
+        file_put_contents($pathToLogfile, chr(10) . 'New entries (' . count($this->_newFiles) . '):' . chr(10), FILE_APPEND);
         if (0 < count($this->_newFiles)) {
             foreach ($this->_newFiles as $i => $filename) {
-                file_put_contents($pathToLogfile, $filename . chr(10), FILE_APPEND);
+                file_put_contents($pathToLogfile, $this->resolveType($filename, "| {$filename}") . chr(10), FILE_APPEND);
                 unset($this->_newFiles[$i]);
             }
         }
 
-        file_put_contents($pathToLogfile, chr(10) . 'Changed files (' . count($this->_changedFiles) . '):' . chr(10), FILE_APPEND);
+        file_put_contents($pathToLogfile, chr(10) . 'Changed entries (' . count($this->_changedFiles) . '):' . chr(10), FILE_APPEND);
         if (0 < count($this->_changedFiles)) {
             foreach ($this->_changedFiles as $i => $filename) {
-                file_put_contents($pathToLogfile, $filename . chr(10), FILE_APPEND);
+                file_put_contents($pathToLogfile, $this->resolveType($filename, "| {$filename}") . chr(10), FILE_APPEND);
                 unset($this->_changedFiles[$i]);
             }
         }
 
-        file_put_contents($pathToLogfile, chr(10) . 'Deleted files (' . count($this->_deletedFiles) . '):' . chr(10), FILE_APPEND);
+        file_put_contents($pathToLogfile, chr(10) . 'Deleted entries (' . count($this->_deletedFiles) . '):' . chr(10), FILE_APPEND);
         if (0 < count($this->_deletedFiles)) {
             foreach ($this->_deletedFiles as $i => $filename) {
-                file_put_contents($pathToLogfile, $filename . chr(10), FILE_APPEND);
+                file_put_contents($pathToLogfile, $this->resolveType($filename, "| {$filename}") . chr(10), FILE_APPEND);
                 unset($this->_deletedFiles[$i]);
             }
         }
